7d66fff9c7bc5b96ffe72407f1207e11dcde4a03
IJTINK7 <ijtink7@mail.ru>
2023-06-15 13:12:32 +0300

    Creating storeRedux.ts file

---
+++ src/components/store/reduxStore/storeRedux.ts
@@ -0,0 +1,12 @@
+import {combineReducers, legacy_createStore as createStore} from "redux";
+import {profileReducer} from "../reducers/profileReducer";
+import {dialogsReducer} from "../reducers/dialogsReducer";
+
+const reducers = combineReducers(
+	{
+		profilePage: profileReducer,
+		dialogsPage: dialogsReducer,
+	}
+);
+export type AppRootStateType = ReturnType<typeof reducers>
+export const store = createStore(reducers);
