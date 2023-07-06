2bc22cbfe771743e30f00590f08045c12f4d2e23
IJTINK7 <ijtink7@mail.ru>
2023-07-06 12:17:50 +0300

    Creating usersReducer.ts file

---
+++ src/components/store/reducers/usersReducer.ts
@@ -0,0 +1,61 @@
+export type UserLocation = {
+	city: string;
+	country: string;
+};
+
+export type UserType = {
+	id: number;
+	photoUrl: string;
+	followed: boolean;
+	fullName: string;
+	status: string;
+	location: UserLocation;
+};
+
+type InitialStateType = {
+	users: UserType[];
+};
+
+type ActionsUsersType = ReturnType<typeof followAC> | ReturnType<typeof unfollowAC> | ReturnType<typeof setUsersAC>
+
+const initialState: InitialStateType = {
+	users: [
+		{ id: 1, photoUrl:"https://drevnerus.ru/drevnerus.ru/public_html/wp-content/uploads/2011/12/volhvy-2.jpg", followed: true, fullName: 'Yulia', status: "", location: { city: "Minsk", country: "Belarus" } },
+		{ id: 2, photoUrl:"https://drevnerus.ru/drevnerus.ru/public_html/wp-content/uploads/2011/12/volhvy-2.jpg", followed: false, fullName: 'Yulia', status: "", location: { city: "Moscow", country: "Russia" } },
+		{ id: 3, photoUrl:"https://drevnerus.ru/drevnerus.ru/public_html/wp-content/uploads/2011/12/volhvy-2.jpg", followed: false, fullName: 'Yulia', status: "", location: { city: "Kiev", country: "Ukraine" } },
+	],
+}
+export const usersReducer = (state: InitialStateType = initialState, action: ActionsUsersType):InitialStateType => {
+	switch (action.type){
+		case "FOLLOW":{
+			return {...state, users: state.users.map(el=> el.id === action.userID ? {...el, followed: false} : el)}
+		}
+		case "UNFOLLOW":{
+			return {...state, users: state.users.map(el=> el.id === action.userID ? {...el, followed: true} : el)}
+		}
+		case "SET-USERS":{
+			return {...state, users: [...action.users]}
+		}
+		default: return state
+	}
+}
+
+export const followAC = (userID: number) =>{
+	return{
+		type: "FOLLOW",
+		userID
+	}as const
+}
+
+export const unfollowAC = (userID: number) =>{
+	return{
+		type: "UNFOLLOW",
+		userID
+	}as const
+}
+export const setUsersAC = (users: UserType[]) =>{
+	return{
+		type: "SET-USERS",
+		users
+	}as const
+}
